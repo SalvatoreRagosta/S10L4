b5ec9454e2d26b25ffb7e06ad66a8f922f411989
Salvatore Ragosta <sasyragosta24@gmail.com>
2024-01-11 16:35:11 +0100

    S10L4

---
+++ src/components/SingleComment.jsx
@@ -0,0 +1,39 @@
+import { Button, ListGroup } from 'react-bootstrap'
+
+const SingleComment = ({ comment }) => {
+  const deleteComment = async (asin) => {
+    try {
+      let response = await fetch(
+        'https://striveschool-api.herokuapp.com/api/comments/' + asin,
+        {
+          method: 'DELETE',
+          headers: {
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTljMTVlYWUwZGQxZDAwMTgyZDE4MzUiLCJpYXQiOjE3MDQ3MjgwNDIsImV4cCI6MTcwNTkzNzY0Mn0.d3NYogX9x1Trv4HDeBugXlpKHp-yZ-GurJVZjxwKc_w',
+          },
+        }
+      )
+      if (response.ok) {
+        alert('La recensione è stata elimata!')
+      } else {
+        throw new Error('La recensione non è stata eliminata!')
+      }
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  return (
+    <ListGroup.Item data-testid="single-comment">
+      {comment.comment}
+      <Button
+        variant="danger"
+        className="ms-2"
+        onClick={() => deleteComment(comment._id)}
+      >
+        Elimina
+      </Button>
+    </ListGroup.Item>
+  )
+}
+
+export default SingleComment
